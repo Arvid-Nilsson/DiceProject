e19c32e8f2d687177a2d22a51e167b48ded227f0
Arvid Nilsson <arvid.nilsson2@elev.ga.ntig.se>
2023-12-05 10:23:01 +0100

    changing rollDice so that it returns a string rather than writes to console

--- Program.cs
+++ Program.cs
@@ -17,42 +17,36 @@ namespace diceroller{
             return sides;
         }
 
-        public Tuple<int, int[]> rollDice(){
+        public string rollDice(){
             Random rnd = new Random();
             int sum = 0;
             int[] rolls = new int[amount];
+            string rollsString = "";
 
             for(int i = 0; i < amount; i++){
                 int roll = rnd.Next(0, sides) + 1;
                 sum = sum + roll;
                 rolls[i] = roll;
+
+                if(i == amount - 1){
+                    rollsString += $"och {roll}";
+                }
+                else if(i == amount - 2){
+                    rollsString += $"{roll} ";
+                }
+                else{
+                    rollsString += $"{roll}, ";
+                }
             } 
-            
-            return new Tuple<int, int[]>(sum, rolls);
+            return $"Du slog {this.getamount()}D{this.getsides()} och fick {rollsString} detta blir totalt {sum}";
         }
     }
 
     class Program{
         public static void Main(string[] args){
             Dice tarning = new Dice(3, 6);
-            var dice = tarning.rollDice();
-            string dicerolls = "";
-            //foreach(int x in dice.Item2){
-            //    dicerolls += $"{x}, ";
-            //}
-            for(int i = 0; i < dice.Item2.Length; i++){
-                int x = dice.Item2[i];
-                if(i == dice.Item2.Length - 1){
-                    dicerolls += $"och {x}";
-                }
-                else if(i == dice.Item2.Length - 2){
-                    dicerolls += $"{x} ";
-                }
-                else{
-                    dicerolls += $"{x}, ";
-                }
-            }
-            Console.WriteLine($"Du slog {tarning.getamount()}D{tarning.getsides()} och fick {dicerolls} detta blir totalt {dice.Item1}");
+            
+            Console.WriteLine(tarning.rollDice());
         }
     }
 }
