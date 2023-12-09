a59cb16d15693f80cd03f7f55a119d142248cdab
Arvid Nilsson <arvid.nilsson2@elev.ga.ntig.se>
2023-12-09 12:03:40 +0100

    Adding comments

--- Program.cs
+++ Program.cs
@@ -5,18 +5,20 @@ namespace diceroller{
         private int amount;
         private int sides;
 
+        //property for amount, checks so that more than 1 dice is rolld
         public int Amount{
             get{return Amount;}
 
             set{
-                if(value >= 2){
+                if(value >= 1){
                     amount = value;
                 }
                 else{
-                    amount = 2;
+                    amount = 1;
                 }
             }
         }
+        //property for sides, checks if the dice has at least 2 sides. 1 side whould be imposible
         public int Sides{
             get{return Amount;}
 
@@ -29,43 +31,39 @@ namespace diceroller{
                 }
             }
         }
-
+        //Constructor for enering sides and ampunt as sepparet intagers
         public Dice(int enterAmount, int enterSides){
             Amount = enterAmount;
             Sides = enterSides;
         }
+        //Constructor for entering a string in the format xDx or xTx even though xTx is wrong
         public Dice(string input){
             string inputLower = input.ToLower();
             int dPosition = inputLower.IndexOf("d");
             int length = inputLower.Length;
             
+            //adds compatability with the format xTx
             if(dPosition == -1 && inputLower.Contains("t") == true){
                 dPosition = inputLower.IndexOf("t");
                 Console.WriteLine("Skriv inte in T, det är D men jag kommer tillåta det den här gången\n");
             }
 
+            //Writing the amount and sides to the private amount and sides through their properties
             Amount = int.Parse(inputLower.Substring(0,dPosition));
             Sides = int.Parse(inputLower.Substring(dPosition + 1, length - dPosition - 1)); 
         }
 
-        public int getamount(){
-            return amount;
-        }
-        public int getsides(){
-            return sides;
-        }
-
+        //Rolls the dice
         public string rollDice(){
             Random rnd = new Random();
             int sum = 0;
-            int[] rolls = new int[amount];
             string rollsString = "";
 
             for(int i = 0; i < amount; i++){
                 int roll = rnd.Next(0, sides) + 1;
                 sum = sum + roll;
-                rolls[i] = roll;
 
+                //Makes the a string of the array of rolls
                 if(i == amount - 1){
                     rollsString += $"och {roll}";
                 }
@@ -76,7 +74,7 @@ namespace diceroller{
                     rollsString += $"{roll}, ";
                 }
             } 
-            return $"Du slog {this.getamount()}D{this.getsides()} och fick {rollsString} detta blir totalt {sum}";
+            return $"Du slog {this.amount}D{this.sides} och fick {rollsString} detta blir totalt {sum}";
         }
     }
 
